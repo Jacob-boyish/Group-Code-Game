781148f61732904f73a54bd5080934ec4dd1104e
Jacob-boyish <jacob.tabernero@stu.sunrisesd.ca>
2025-03-23 20:38:12 -0700

    Add files via upload
    This is the game mechanic for placing blocks that is a component for the player. It can place and destroy blocks.
    
    Things needed on your side:
    Block prefab with collider
    ShootingPoint empty Game Object that is child of camera and place directly in front and a bit above the camera
    Tags called 'Block' and 'Ground'
    
    Find out how your player can look downwards more freely because this hinders block placing, preventing nerd poling.

---
+++ BuildSystem.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderKeywordFilter;
+using UnityEngine;
+
+public class BuildSystem : MonoBehaviour
+{
+    public Transform shootingPoint;
+    public GameObject blockObject;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) // if left-click, place block
+        {
+            BuildBlock(blockObject);
+        }
+        if (Input.GetMouseButtonDown(1)) // if right-click, remove block
+        {
+            DestroyBlock();
+        }
+
+    }
+
+    void BuildBlock(GameObject block)
+    {
+
+
+        if(Physics.Raycast(shootingPoint.position, shootingPoint.forward, out RaycastHit hitInfo))
+        {
+
+            if (hitInfo.transform.tag == "Block")
+            {
+                Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x + hitInfo.normal.x / 2), Mathf.RoundToInt(hitInfo.point.y + hitInfo.normal.y / 2), Mathf.RoundToInt(hitInfo.point.z + hitInfo.normal.z / 2));
+                Instantiate(block, spawnPosition, Quaternion.identity);
+            }
+            if (hitInfo.transform.tag == "Ground") // if place block on ground, keep it from being inside ground
+            {
+                Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x + hitInfo.normal.x / 2), Mathf.RoundToInt(hitInfo.point.y + hitInfo.normal.y / 2), Mathf.RoundToInt(hitInfo.point.z + hitInfo.normal.z / 2));
+                Instantiate(block, spawnPosition, Quaternion.identity);
+            }
+            else
+            {
+                Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x), Mathf.RoundToInt(hitInfo.point.y), Mathf.RoundToInt(hitInfo.point.z));
+                Instantiate(block, spawnPosition, Quaternion.identity);
+            }
+        }
+    }
+
+    void DestroyBlock()
+    {
+        if (Physics.Raycast(shootingPoint.position, shootingPoint.forward, out RaycastHit hitInfo))
+        {
+            if (hitInfo.transform.tag == "Block")
+            {
+                Destroy(hitInfo.transform.gameObject);
+            }
+        }
+    }
+}
